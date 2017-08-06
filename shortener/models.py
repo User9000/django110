7cb1dcc7c481d3126eec1a271d0782d0cd3183f8
Carlo Lam <calam12@gmail.com>
2017-08-05 17:44:54 -0700

    third commit

--- shortener/models.py
+++ shortener/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 #Create your models here.
 class KirrURL(models.Model):
-    url = models.CharField(max_length=220,)
-    shortcode = models.CharField(max_length=15, unique=True)
+    url         = models.CharField(max_length=220,)
+    shortcode   = models.CharField(max_length=15, unique=True)
+    updated     = models.DateTimeField(auto_now = True) #everytime the model is saved
+    timestamp   = models.DateTimeField(auto_now=True) #when model was created
+    #mpty_datetime = models.DateTimeFuled ( auto_now= False, auto_now_add = False)
     #shortcode = models.CharField(max_length=15, null=True,blank = False) #empty in db is okay
     #shortcode = models.CharField(max_length=15,default='carlodefault')
 
