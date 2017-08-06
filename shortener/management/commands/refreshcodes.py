1377456a8f8511a339ee42532e87d35295155348
Carlo Lam <calam12@gmail.com>
2017-08-06 16:11:32 -0700

    seventh commit

--- shortener/management/commands/refreshcodes.py
+++ shortener/management/commands/refreshcodes.py
@@ -1,12 +1,17 @@
 
 
 from django.core.management.base import BaseCommand, CommandError
-
 from shortener.models import KirrURL
+
+
 
 class Command(BaseCommand):
     help = 'Refreshes all KirrURL shortcodes'
 
+    def add_arguments(self,parser):
+        parser.add_argument('items', type=int)
+
 
     def handle(self,*args,**options):
-        return KirrURL.objects.refresh_shortcodes()
+       # print(options)
+        return KirrURL.objects.refresh_shortcodes(items=options['items'])
