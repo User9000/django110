2afe8702b9b2332f02faa552dcbed32f57b33174
Carlo Lam <calam12@gmail.com>
2017-08-05 19:13:27 -0700

    fourth commit

--- shortener/models.py
+++ shortener/models.py
@@ -1,6 +1,17 @@
+import random
+import string
 from django.db import models
 
 #Create your models here.
+def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
+    #new_code  = ''
+    #for _ in range(size):
+    #new_code += random.choice(chars)
+    #return new_code
+    return ''.join(random.choice(chars) for _ in range(size))
+
+
+
 class KirrURL(models.Model):
     url         = models.CharField(max_length=220,)
     shortcode   = models.CharField(max_length=15, unique=True)
@@ -10,12 +21,19 @@ class KirrURL(models.Model):
     #shortcode = models.CharField(max_length=15, null=True,blank = False) #empty in db is okay
     #shortcode = models.CharField(max_length=15,default='carlodefault')
 
+    def save(self, *args, **kwargs):
+        print('something')
+        self.shortcode = code_generator()
+        super(KirrURL,self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.url)
 
     def __unicode__(self):
         return str(self.url)
+    
+
+    
 
 
 '''
