d134b416d658ab34967fa70a73d8e835a5e5e869
Carlo Lam <calam12@gmail.com>
2017-08-05 19:59:45 -0700

    fifth commit

--- shortener/models.py
+++ shortener/models.py
@@ -1,20 +1,13 @@
-import random
-import string
-from django.db import models
+
+from django.db import models 
+from .utils import code_generator, create_shortcode
 
 #Create your models here.
-def code_generator(size=6, chars=string.ascii_lowercase + string.digits):
-    #new_code  = ''
-    #for _ in range(size):
-    #new_code += random.choice(chars)
-    #return new_code
-    return ''.join(random.choice(chars) for _ in range(size))
-
 
 
 class KirrURL(models.Model):
     url         = models.CharField(max_length=220,)
-    shortcode   = models.CharField(max_length=15, unique=True)
+    shortcode   = models.CharField(max_length=15, unique=True, blank= True)
     updated     = models.DateTimeField(auto_now = True) #everytime the model is saved
     timestamp   = models.DateTimeField(auto_now=True) #when model was created
     #mpty_datetime = models.DateTimeFuled ( auto_now= False, auto_now_add = False)
@@ -22,9 +15,9 @@ class KirrURL(models.Model):
     #shortcode = models.CharField(max_length=15,default='carlodefault')
 
     def save(self, *args, **kwargs):
-        print('something')
-        self.shortcode = code_generator()
-        super(KirrURL,self).save(*args, **kwargs)
+        if  not self.shortcode is None or self.shortcode == "":
+            self.shortcode = create_shortcode(self)
+            super(KirrURL,self).save(*args, **kwargs)
 
     def __str__(self):
         return str(self.url)
@@ -32,9 +25,6 @@ class KirrURL(models.Model):
     def __unicode__(self):
         return str(self.url)
     
-
-    
-
 
 '''
 python manage.py makemigrations
