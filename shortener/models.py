c935fb14f8c81230de234f17b236d7ecb1a9bf43
Carlo Lam <calam12@gmail.com>
2017-08-05 17:32:52 -0700

    second commit

--- shortener/models.py
+++ shortener/models.py
@@ -3,10 +3,13 @@ from django.db import models
 #Create your models here.
 class KirrURL(models.Model):
     url = models.CharField(max_length=220,)
+    shortcode = models.CharField(max_length=15, unique=True)
+    #shortcode = models.CharField(max_length=15, null=True,blank = False) #empty in db is okay
+    #shortcode = models.CharField(max_length=15,default='carlodefault')
+
 
     def __str__(self):
         return str(self.url)
-    
 
     def __unicode__(self):
         return str(self.url)
@@ -14,7 +17,6 @@ class KirrURL(models.Model):
 
 '''
 python manage.py makemigrations
-
 python manage.py migrate
 
 
