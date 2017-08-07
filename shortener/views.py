bad98541a3d50454c934419432372322863d6fc9
Carlo Lam <calam12@gmail.com>
2017-08-06 17:39:37 -0700

    ningth commit

--- shortener/views.py
+++ shortener/views.py
@@ -1,19 +1,37 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse,HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
 from django.views import View
 
+from .models import KirrURL
 # Create your views here.
 
 def kirr_redirect_view(request,shortcode=None,*args, **kwargs):
-    print(request.user)
-    print(request.user.is_authenticated())
-    print(args)
-    print(kwargs)
-    return HttpResponse("hello {sc}".format(sc=shortcode))
+    #print(request.user)
+    #print(request.user.is_authenticated())
+    #print(args)
+    #print(kwargs)
+    #obj = KirrURL.objects.get(shortcode=shortcode)
+    print(request.method)
+    obj = get_object_or_404(KirrURL,shortcode=shortcode)
+    
+    #obj_url = obj.url
+    ''' try:
+        obj = KirrURL.objects.get(shortcode=shortcode)
+    except:
+        obj = KirrURL.objects.all().first() '''
+    ''' obj_url = None
+    qs = KirrURL.objects.filter(shortcode__iexact=shortcode.upper())
+    if qs.exists() and qs.count() == 1:
+        obj = qs.first()
+        obj_url = obj.url '''
+    return HttpResponseRedirect(obj.url)
+    #return HttpResponse("hello {sc}".format(sc=obj.url))
 
 
 class KirrCBView(View):
     def get(self,request,shortcode=None,*args,**kwargs):
+        obj = get_object_or_404(KirrURL,shortcode=shortcode)
         print(args)
         print(kwargs)
-        return HttpResponse("hello again {sc}".format(sc=shortcode))
+        #return HttpResponse("hello again {sc}".format(sc=shortcode))
+        return HttpResponseRedirect(obj.url)
