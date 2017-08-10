e7c6943ac3d942a618be8d6647530ebe95cbc2eb
Carlo Lam <calam12@gmail.com>
2017-08-09 22:37:13 -0700

    added stuff to the forms

--- shortener/views.py
+++ shortener/views.py
@@ -2,17 +2,33 @@ from django.http import HttpResponse,HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.views import View
 
+from .forms import SubmitUrlForm
 from .models import KirrURL
+
 # Create your views here.
 
 class HomeView(View):
     def get(self,request, *args,**kwargs):
-        return render(request,"shortener/home.html", {}) #
+        the_form = SubmitUrlForm()
+        context = {
+            "title":"Submit Url",
+            "form":the_form,
+
+
+        }
+        return render(request,"shortener/home.html", context) #
     
     def post(self, request, *args, **kwargs):
-        print(request.POST)
-        print(request.POST.get('url'))
-        return render(request, "shortener/home.html",{})
+       # print(request.POST)
+       # print(request.POST.get('url'))
+        form = SubmitUrlForm(request.POST)
+
+        if form.is_valid():
+            print(form.cleaned_data)
+        context ={
+            "form": form
+        }
+        return render(request, "shortener/home.html",context)
 
 
 
